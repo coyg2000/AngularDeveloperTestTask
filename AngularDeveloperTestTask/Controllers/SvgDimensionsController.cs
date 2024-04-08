ef81ffcfe487e1d09bf05a3aa1b2fff76ecada05
coyg2000 <sr47050@ubt-uni.net>
2024-04-09 00:28:40 +0200

    clean code

--- AngularDeveloperTestTask/Controllers/SvgDimensionsController.cs
+++ AngularDeveloperTestTask/Controllers/SvgDimensionsController.cs
@@ -29,46 +29,7 @@ namespace AngularDeveloperTestTask.Controllers
             var jsonContent = System.IO.File.ReadAllText(jsonFilePath);
             return Ok(jsonContent);
         }
-        //qekjo punon mire 
-        //[HttpPut]
-        //public async Task<IActionResult> UpdateDimensions([FromBody] SvgDimensionsModel model)
-        //{
-        //    try
-        //    {
-        //        var jsonFilePath = Path.Combine(_webHostEnvironment.WebRootPath, "svgDimensions.json");
 
-        //        // Serialize the model to JSON
-        //        var jsonContent = JsonSerializer.Serialize(model);
-
-        //        // Write the JSON content to the file
-        //        await System.IO.File.WriteAllTextAsync(jsonFilePath, jsonContent, Encoding.UTF8);
-
-        //        return Ok("Dimensions updated successfully");
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        return StatusCode(500, "An error occurred while updating dimensions");
-        //    }
-        //}
-
-        //qekjo e ka ni prb cannot convert json to string 
-        //[HttpPut]
-        //public async Task<IActionResult> UpdateDimensions([FromBody] string jsonContent)
-        //{
-        //    try
-        //    {
-        //        var jsonFilePath = Path.Combine(_webHostEnvironment.WebRootPath, "svgDimensions.json");
-
-        //        // Write the JSON content to the file
-        //        await System.IO.File.WriteAllTextAsync(jsonFilePath, jsonContent, Encoding.UTF8);
-
-        //        return Ok("Dimensions updated successfully");
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        return StatusCode(500, "An error occurred while updating dimensions");
-        //    }
-        //}
 
         [HttpPut]
         public async Task<IActionResult> UpdateDimensions([FromBody] JsonElement jsonContent)
@@ -92,20 +53,6 @@ namespace AngularDeveloperTestTask.Controllers
         }
 
 
-        //[HttpPut]
-        //public async Task<IActionResult> UpdateDimensions(JsonDocument updatedDimensions)
-        //{
-        //    using (var streamWriter = new StreamWriter(_jsonFilePath))
-        //    {
-        //        await JsonSerializer.SerializeAsync(streamWriter.BaseStream, updatedDimensions);
-        //        return Ok();
-        //    }
-        //}
     }
-    //public class SvgDimensionsModel
-    //{
-    //    public int Width { get; set; }
-    //    public int Height { get; set; }
-    //}
 }
 
